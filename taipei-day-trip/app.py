d178a3beee1a7e67b0f4bd88209c062f42ee45c6
jackylailai <jkdd84@gmail.com>
2023-09-02 09:13:21 +0800

    back to .env

--- taipei-day-trip/app.py
+++ taipei-day-trip/app.py
@@ -7,13 +7,13 @@ import os.path
 # logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
 # 載入.env文件中的環境變數
-# load_dotenv()
+load_dotenv()
 # 從環境變數中讀取資料庫配置
 db_config = {
-    "host": os.environ.get('DB_HOST'),
-    "user": os.environ.get("DB_USER"),
-    "password": os.environ.get("DB_PASSWORD"),
-    "database": os.environ.get("DB_DATABASE"),
+    "host": os.getenv("DB_HOST"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_DATABASE"),
 }
 
 connection = mysql.connector.connect(**db_config)
