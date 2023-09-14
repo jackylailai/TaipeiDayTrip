c09f559189cd7239d7e3ed31354a8220f5e1c473
jackylailai <jkdd84@gmail.com>
2023-09-14 22:14:49 +0800

    finished image below button and color insertion before teacher tutoring

--- taipei-day-trip/static/js/attraction.js
+++ taipei-day-trip/static/js/attraction.js
@@ -162,54 +162,127 @@ morningBtn.style.backgroundColor = "#FFFF";
 fareText.textContent = "新台幣2500元";
 });
 
-//按鈕跟小圓點一起處理
+// //按鈕跟小圓點一起處理
+// function imageButton() {
+//     const leftButton = document.querySelector('.left-button');
+//     const rightButton = document.querySelector('.right-button');
+//     const imageContainer = document.querySelector('.image-container');
+//     const imageDots = document.querySelectorAll('.image-dot');
+
+//     const circle = document.querySelector('.circle');
+//     const circleA = circle.children;
+//     console.log(`${circle}and${circleA}`)
+
+//     let currentIndex = 0;
+
+//     function updateDisplayedImage(index) {
+//         const images = document.querySelectorAll('.image');
+
+//         for (let i = 0; i < images.length; i++) {
+//             const img_display = images[i];
+//             if (i === index) {
+//                 img_display.style.display = 'block';
+//                 console.log(`${i}第幾張圖片`)
+//                 // imageDots[i].classList.add('active');
+//             } else {
+//                 img_display.style.display = 'none';
+//                 // imageDots[i].classList.remove('active');
+//             }
+//         }
+//         for(let i =0;i<images.length;i++){
+//             let aNode = document.createElement('a');
+
+//             aNode.setAttribute('index',i);
+//             circle.appendChild(aNode);
+//         }
+//         imageDots.forEach((dot, i) => {
+//             if (i === index) {
+//                 console.log("進for處理dot",dot)
+//                 dot.style.backgroundColor = "#000";
+//                 // dot.classList.add('active');
+//             } else {
+//                 dot.style.backgroundColor = "transparent";
+//                 // dot.classList.remove('active');
+//             }
+//         });
+//         function circlechange(currentIndex){
+//             console.log("進入處理原點的func")
+//             for(let i = 0;i <circleA.length;i++){
+//                 console.log(`${i}：i的值`)
+//                 circleA[i].className=''
+
+//             }
+//             circleA[currentIndex].className='hover';
+//         }
+//         circlechange(currentIndex);
+//         currentIndex = index;
+//     }
+
+//     leftButton.addEventListener('click', function () {
+//         currentIndex = (currentIndex - 1 + imageContainer.children.length) % imageContainer.children.length;
+//         updateDisplayedImage(currentIndex);
+//     });
+
+//     rightButton.addEventListener('click', function () {
+//         currentIndex = (currentIndex + 1) % imageContainer.children.length;
+//         updateDisplayedImage(currentIndex);
+//     });
+//     // 初始化第一個圖片和圖片原點的狀態
+//     updateDisplayedImage(currentIndex);
+// }
 function imageButton() {
     const leftButton = document.querySelector('.left-button');
     const rightButton = document.querySelector('.right-button');
     const imageContainer = document.querySelector('.image-container');
-    const imageDots = document.querySelectorAll('.image-dot');
+    const images = document.querySelectorAll('.image');
+    const imageButton = document.querySelector('.image-dot');
 
     let currentIndex = 0;
 
-    function updateDisplayedImage(index) {
-        const images = document.querySelectorAll('.image');
+    // 動態生成圖片原點（circlea）
+    const circle = document.querySelector('.circle');
+    console.log(`image length${images.length}`);
+    for (let i = 0; i < images.length; i++) {
+        const circleA = document.createElement('div');
+        circleA.className = 'circle-a';
+        circleA.setAttribute('index', i);
+        circle.appendChild(circleA);
+        console.log(circleA);
+    }
 
+    const circleAElements = document.querySelectorAll('.circle-a');
+
+    function updateDisplayedImage(index) {
+        console.log("進入funcdisplay circleAele為",circleAElements);
         for (let i = 0; i < images.length; i++) {
-            console.log("進for處理image",i)
             const img_display = images[i];
+            const circle_display = circleAElements[i];
             if (i === index) {
+                console.log("進入回圈選圖")
                 img_display.style.display = 'block';
-                // imageDots[i].classList.add('active');
+                console.log(circleAElements[i]);
+                console.log(`circleaelements${circleAElements[i]}image`);
+                circle_display.style.backgroundColor = "#FFFF";
             } else {
                 img_display.style.display = 'none';
-                // imageDots[i].classList.remove('active');
+                circleAElements[i].style.backgroundColor= "#000";
             }
         }
 
-        imageDots.forEach((dot, i) => {
-            if (i === index) {
-                console.log("進for處理dot",dot)
-                dot.style.backgroundColor = "#000";
-                // dot.classList.add('active');
-            } else {
-                dot.style.backgroundColor = "transparent";
-                // dot.classList.remove('active');
-            }
-        });
         currentIndex = index;
     }
 
     leftButton.addEventListener('click', function () {
-        currentIndex = (currentIndex - 1 + imageContainer.children.length) % imageContainer.children.length;
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
         updateDisplayedImage(currentIndex);
     });
 
     rightButton.addEventListener('click', function () {
-        currentIndex = (currentIndex + 1) % imageContainer.children.length;
+        currentIndex = (currentIndex + 1) % images.length;
         updateDisplayedImage(currentIndex);
     });
 
-    // 初始化第一個圖片和圖片原點的狀態
+    // 初始化第一張圖片和圖片原點的狀態
     updateDisplayedImage(currentIndex);
 }
 
